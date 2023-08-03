dc707c1fe11947edb1291a3d1993f5c1124bf448
Alin Jerpelea <alin.jerpelea@sony.com>
2023-08-03 13:49:29 +0200

    build_shared_vars.sh: bump kernel version
    the latest devices use linux 5.15 kernel
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- build_shared_vars.sh
+++ build_shared_vars.sh
@@ -47,10 +47,10 @@ PLATFORMS="nagara yodo"
 # Mkdtimg tool
 MKDTIMG=$ANDROID_ROOT/out/host/linux-x86/bin/mkdtimg
 
-KERNEL_TOP=$ANDROID_ROOT/kernel/sony/msm-5.10
+KERNEL_TOP=$ANDROID_ROOT/kernel/sony/msm-5.15
 # $KERNEL_TMP sub dir per script
 c=${0##*-}
-KERNEL_TMP=$ANDROID_ROOT/out/kernel-5.10/${c%%.sh}
+KERNEL_TMP=$ANDROID_ROOT/out/kernel-5.15/${c%%.sh}
 
 export PATH=$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
 export PATH=$PATH:$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
