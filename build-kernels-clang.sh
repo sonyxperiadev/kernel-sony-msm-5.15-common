f2614db5b0b401164599e3d1dcd619d8beb0be49
Pavel Dubrova <pashadubrova@gmail.com>
2025-11-25 04:36:44 +0200

    build-kernels-clang: Add Android 15 Clang version
    Add clang-r522817 for Android V release.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- build-kernels-clang.sh
+++ build-kernels-clang.sh
@@ -45,6 +45,7 @@ trap 'echo -e "\n${0##*/} execution was interrupted by Ctrl+C."; exit 1' SIGINT
 #   - Ensure that ANDROID_ROOT is defined before referencing toolchain paths.
 # -------------------------------------------------------------------------
 declare -A CLANG_VERSIONS=(
+    ['15']="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r522817/bin/"
     ['14']="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r487747c/bin/"
     ['13']="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r450784d/bin/"
     ['12']="$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r416183b/bin/"
